0d9fdda661d5c67d4133b7832efb600aec135cb6
TJ Ryan <tjryan314@gmail.com>
2023-08-14 17:56:20 -0500

    fixed my problem by importing satData.js to Buttons.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import satData from "./components/satData";
 function App() {
 
   const [sat, setSat] = useState(satData);
-  const displaySats = [... new Set(satData.map((data) => data.orbitType))];
+  const displaySats = [...new Set(satData.map((data) => data.orbitType))];
   const filterByType = (currentType) => {
     const displaySats = satData.filter((newSatDisplay) => {
       return newSatDisplay.orbitType === currentType;
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import satData from "./satData";
 
 const Buttons = ({filterByType, setSat, displaySats}) => {
   return (
